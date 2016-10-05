f41abe1dff0cfba3735c61de4ac50f36b284a536
George Hawkins <george-hawkins@users.noreply.github.com>
2016-10-05 22:09:59 +0200

    Factored out common echo and reverse code into basic-ws.js.

--- src/main/resources/static/js/reverse.js
+++ src/main/resources/static/js/reverse.js
@@ -1,61 +1,10 @@
-var ws = null;
-
-function setConnected(connected) {
-    document.getElementById('connect').disabled = connected;
-    document.getElementById('disconnect').disabled = !connected;
-    document.getElementById('reverse').disabled = !connected;
-}
-
-function connect() {
-    var target = document.getElementById('target').value;
-    ws = new WebSocket(target);
-    ws.onopen = function () {
-        setConnected(true);
-        log('Info: WebSocket connection opened.');
-    };
-    ws.onmessage = function (event) {
-        log('Received: ' + event.data);
-    };
-    ws.onclose = function () {
-        setConnected(false);
-        log('Info: WebSocket connection closed.');
-    };
+// TODO: why does echo use our nice SockJS while reverse depends on browser web socket support (and the updateTarget() logic)?
+function createWebSocket(target) {
+    return new WebSocket(target);
 }
 
 function updateTarget() {
-    if (window.location.protocol == 'http:') {
-        document.getElementById('target').value = 'ws://' + window.location.host + document.getElementById('target').value;
-    } else {
-        document.getElementById('target').value = 'wss://' + window.location.host + document.getElementById('target').value;
-    }
-}
+    var scheme = (window.location.protocol == 'http:') ? 'ws://' : 'wss://';
 
-function disconnect() {
-    if (ws != null) {
-        ws.close();
-        ws = null;
-    }
-    setConnected(false);
-}
-
-function reverse() {
-    if (ws != null) {
-        var message = document.getElementById('message').value;
-        log('Sent: ' + message);
-        ws.send(message);
-    } else {
-        alert('WebSocket connection not established, please connect.');
-    }
-}
-
-function log(message) {
-    var console = document.getElementById('console');
-    var p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(message));
-    console.appendChild(p);
-    while (console.childNodes.length > 25) {
-        console.removeChild(console.firstChild);
-    }
-    console.scrollTop = console.scrollHeight;
+    document.getElementById('target').value = scheme + window.location.host + document.getElementById('target').value;
 }
